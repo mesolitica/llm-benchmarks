da2c6a14a6f19dc488f3217de80ca8046b9122cd
theblackcat102 <theblackcat102@github.com>
2024-04-01 19:26:56 +0800

    [fix] missing parsing choice, better presentation

--- BM-PT3/claude-3-sonnet-20240229/claude3_eval_bm_pt3.py
+++ BM-PT3/claude-3-sonnet-20240229/claude3_eval_bm_pt3.py
@@ -81,59 +81,57 @@ soalan: {row['soalan']}
     """
     return prompt.strip()
 
-num_shot = 3
-model_name = "claude-3-haiku-20240307"
-model_name = "claude-3-sonnet-20240229"
-model_name = "claude-3-opus-20240229"
-output_filename = 'bm_pt3-shot-{}-'.format(num_shot)+model_name+'.jsonl'
-inferenced = set()
-if os.path.exists(output_filename):
-    with open(output_filename) as fopen:
-        for no, l in enumerate(fopen):
-            l = json.loads(l)
-            inferenced.add(l['question']['no'])
+for num_shot in [0,1,3]:
+    model_name = "claude-3-sonnet-20240229"
+    output_filename = 'bm_pt3-shot-{}-'.format(num_shot)+model_name+'.jsonl'
+    inferenced = set()
+    if os.path.exists(output_filename):
+        with open(output_filename) as fopen:
+            for no, l in enumerate(fopen):
+                l = json.loads(l)
+                inferenced.add(l['question']['no'])
 
-for idx, question in enumerate(tqdm(questions, dynamic_ncols=True)):
-    if question['no'] in inferenced:
-        continue
+    for idx, question in enumerate(tqdm(questions, dynamic_ncols=True)):
+        if question['no'] in inferenced:
+            continue
 
-    prompt = convert_prompt(question)
-    if num_shot > 0:
-        shots = random.sample([index for index in range(len(questions)) if index != idx], num_shot)
-        few_shots = []
-        for order, shot in enumerate(shots):
-            few_shots.append(f'Contoh soalan {order + 1}\n' + \
-                             convert_prompt(questions[shot], answer=True)
-                             )
-        prompt = '\n\n'.join(few_shots)+'\n\nSekarang jawab soalan ini\n'+prompt
-    response = prompt_answer(prompt, model_name=model_name, temperature=0)
-    response['question'] = question
-    with open(output_filename, 'a') as fout:
-        fout.write(json.dumps(response) + '\n')
+        prompt = convert_prompt(question)
+        if num_shot > 0:
+            shots = random.sample([index for index in range(len(questions)) if index != idx], num_shot)
+            few_shots = []
+            for order, shot in enumerate(shots):
+                few_shots.append(f'Contoh soalan {order + 1}\n' + \
+                                convert_prompt(questions[shot], answer=True)
+                                )
+            prompt = '\n\n'.join(few_shots)+'\n\nSekarang jawab soalan ini\n'+prompt
+        response = prompt_answer(prompt, model_name=model_name, temperature=0)
+        response['question'] = question
+        with open(output_filename, 'a') as fout:
+            fout.write(json.dumps(response) + '\n')
 
-correct, total = 0, 0
-ans_match = re.compile(r'Jawapan: [A-B].')
-base_answer = re.compile(r'[A-D]. ')
-ialah_answer = re.compile(r'ialah:\n[A-D].')
-with open(output_filename) as f:
-    for line in f:
-        payload = json.loads(line)
-        msg = payload['message']
-        if ans_match.search(msg):
-            found = ans_match.search(msg).group(0)
-            response = found.split('.')[0][-1]
-        elif ialah_answer.search(msg):
-            found = ialah_answer.search(msg).group(0)
-            response = found.split('.')[0][-1]
-        elif base_answer.search(msg):
-            found = base_answer.search(msg).group(0)
-            response = found.split('.')[0][-1]
-        else:
-            response = msg.split('.')[0][-1]
-        if response not in ['A', 'B', 'C', 'D']:
-            print(msg)
-            print('----------------')
-        label = payload['question']['jawapan']
-        correct += response == label
-        total += 1
-print(correct, total, 100*correct / total)
+    correct, total = 0, 0
+    ans_match = re.compile(r'Jawapan: [A-D].')
+    base_answer = re.compile(r'[A-D]. ')
+    ialah_answer = re.compile(r'ialah:\n[A-D].')
+    with open(output_filename) as f:
+        for line in f:
+            payload = json.loads(line)
+            msg = payload['message']
+            if ans_match.search(msg):
+                found = ans_match.search(msg).group(0)
+                response = found.split('.')[0][-1]
+            elif ialah_answer.search(msg):
+                found = ialah_answer.search(msg).group(0)
+                response = found.split('.')[0][-1]
+            elif base_answer.search(msg):
+                found = base_answer.search(msg).group(0)
+                response = found.split('.')[0][-1]
+            else:
+                response = msg.split('.')[0][-1]
+            if response not in ['A', 'B', 'C', 'D']:
+                print(msg)
+                print('----------------')
+            label = payload['question']['jawapan']
+            correct += response == label
+            total += 1
+    print(correct, total, 100*correct / total)
